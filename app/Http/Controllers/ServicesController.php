1ba0d0e62bf6e6070d3b5f3dd5d7acb90e9565b5
vaskita101 <vaska.r02@gmail.com>
2017-12-27 23:18:14 +0400

    first commit

---
+++ app/Http/Controllers/ServicesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services;
+use Illuminate\Http\Request;
+
+class ServicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Services  $services
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Services $services)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Services  $services
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Services $services)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Services  $services
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Services $services)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Services  $services
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Services $services)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SocialsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Socials;
+use Illuminate\Http\Request;
+
+class SocialsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Socials  $socials
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Socials $socials)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Socials  $socials
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Socials $socials)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Socials  $socials
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Socials $socials)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Socials  $socials
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Socials $socials)
+    {
+        //
+    }
+}
